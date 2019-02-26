3d92670b736884eb3c4b0ea017f27f6d3081a89d
Kishan <patelk6@students.rowan.edu>
2019-02-26 14:07:21 -0500

    More stuff

--- MainHub.py
+++ MainHub.py
@@ -1,12 +1,26 @@
 #Python 3.6
 #Install PyQt5==5.9.2
 #https://build-system.fman.io/pyqt5-tutorial
-from PyQt5.QtWidgets import QApplication, QLabel
-
+from PyQt5.QtWidgets import *
 app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+button = QPushButton('Top')
+layout.addWidget(button)
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
 
-label = QLabel('Hello World!')
 
-label.show()
 
+def on_button_clicked():
+	alert = QMessageBox()
+	alert.setText('Test Click')
+	alert.exec_()
+
+button.clicked.connect(on_button_clicked)
 app.exec_()
+
+
+
+
